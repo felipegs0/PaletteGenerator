085ae8fdaf08e0badad95590d4f99c3bf9258221
Felipe Gomes <felipegstech@gmail.com>
2026-03-29 12:44:52 -0300

    feat: Added button to copy CSS code.

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,18 @@ function App() {
   const [type, setType] = useState<PaletteType>("mono");
 
   const generateRandom = (type: PaletteType) => {
-    setPalette(generatePalette(type))
-    console.log(palette)
-  }
+    setPalette(generatePalette(type));
+    console.log(palette);
+  };
+
+  const exportCSS = () => {
+    let variables = "";
+    for(let i = 0; i < palette.length; i++) {
+      variables += `   --color-${i + 1}: ${palette[i]}; \n`
+    }
+    const css = `:root {\n${variables}}`
+    navigator.clipboard.writeText(css);
+  };
 
   return (
     <>
@@ -32,35 +41,41 @@ function App() {
               setType(e.target.value as PaletteType)
             }
           >
-            <option value="mono" selected>Monocromático</option>
-            <option value="complementary">Complementar</option>
-            <option value="triad">
-              Tríade
+            <option value="mono" selected>
+              Monocromático
             </option>
+            <option value="complementary">Complementar</option>
+            <option value="triad">Tríade</option>
             <option value="analog">Análogo</option>
           </select>
-          <button className="bg-[#f89917] py-3 rounded-md font-semibold cursor-pointer" onClick={() => generateRandom(type)}>
+          <button
+            className="bg-[#f89917] py-3 rounded-md font-semibold cursor-pointer"
+            onClick={() => generateRandom(type)}
+          >
             Gerar Paleta
           </button>
         </div>
         <div className="bg-[#141821] flex flex-col h-[85vh] border border-[#ffffff13] rounded-br-md rounded-tr-md">
           <div className="grid grid-cols-4 gap-2 w-270 p-7 h-250 ">
-            { palette.map((color, i) => (
+            {palette.map((color, i) => (
               <ColorCard key={i} color={color} />
-            )) }
+            ))}
           </div>
           <div className="bg-[#141821] flex flex-col border-t border-[#ffffff1f] p-7">
             <p className="text-white text-xl font-semibold mb-3">
               Exportar CSS:
             </p>
-            <div className="bg-[#141821] flex flex-col p-5 border border-[#ffffff23] rounded-md">
+            <div className="bg-[#141821] flex flex-col p-5 border border-[#ffffff23] rounded-md relative">
               <p className="text-white font-mono">:root {" { "}</p>
               <div className="flex flex-col pl-5">
-                { palette.map((color, i) => (
-                  <ColorRoot num={i} color={color}/>
-                )) }
+                {palette.map((color, i) => (
+                  <ColorRoot num={i} color={color} />
+                ))}
               </div>
               <p className="text-white font-mono">{" } "}</p>
+              <button className="border border-[#ffffff23] text-white font-semibold text-md px-6 py-2 bottom-3 right-3 rounded-md absolute cursor-pointer" onClick={exportCSS}>
+                Copiar Código
+              </button>
             </div>
           </div>
         </div>
--- src/components/ColorRoot.tsx
+++ src/components/ColorRoot.tsx
@@ -1,7 +1,7 @@
 function ColorRoot({ color, num }: { color: string, num: number }) {
   return (
     <p className="text-[#f89917] font-mono">
-      --color-{num}: <span className="text-white">{color};</span>
+      --color-{num + 1}: <span className="text-white">{color};</span>
     </p>
   );
 }
